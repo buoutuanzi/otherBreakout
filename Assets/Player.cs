6393acca0c2f032566dac5cc53bdd2bde5fc155c
zhangyiming <zhangyiming@aobi.com>
2023-07-19 18:50:14 +0800

    設置了背景，平臺，以及初始毬

---
+++ Assets/Player.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    private float movementX;
+    public float speed;
+    public float bound;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMove(InputValue moveVal)
+    {
+        Vector2 movementVector = moveVal.Get<Vector2>();
+        movementX = movementVector.x;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveDistance = new Vector3(movementX, 0, 0).normalized * speed;
+        float newPositionX = transform.position.x + moveDistance.x;
+        if (newPositionX < -bound)
+        {
+            transform.position = new Vector3(-bound, transform.position.y, transform.position.z);
+        } else if (newPositionX > bound)
+        {
+            transform.position = new Vector3(bound, transform.position.y, transform.position.z);
+        } else
+        {
+            transform.position += moveDistance;
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
