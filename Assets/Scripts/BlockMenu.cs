a0156ebfddfce6e5b164dc7df06c3d97e5ff29c3
zhangyiming <zhangyiming@aobi.com>
2023-07-20 19:15:35 +0800

    自动生成砖块布局

---
+++ Assets/Scripts/BlockMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockMenu : MonoBehaviour
+{
+    public List<GameObject> Blocks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float Y = 4f;
+        
+        for (int i = 0; i < 3; i++)
+        {
+            float X = -7.5f;
+            for (int j = 0; j < 7; j++)
+            {
+                Vector3 relativePos = new Vector3(X, Y, 0);
+                GameObject instance = Instantiate(Blocks[(i + j)%3], transform);
+                instance.transform.position = relativePos;
+                X += 2.5f;
+            }
+            Y -= 2f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
